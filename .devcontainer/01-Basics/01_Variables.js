116cb3de9680ad480ff6b826755677f6013cbb50
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-04-05 20:48:00 +0000

    datatype

--- .devcontainer/01-Basics/01_Variables.js
+++ .devcontainer/01-Basics/01_Variables.js
@@ -1,11 +1,12 @@
-const accountId = 12345
-let accountEmail = "divyanshu@gmail.com"
-var accountPassword = "123456"
-accountCity = "delhi"
+//const accountId = 12345
+//let accountEmail = "divyanshu@gmail.com"
+//var accountPassword = "123456"
+//accountCity = "delhi"
 
 //accountId = 3
 
-console.log(accountId);
+//console.log(accountId);
 //accountEmail = "boy@uur"
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+//console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log("divyanshunode")
---
+++ .devcontainer/01-Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+//alert( 3+3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("divyanshu")
+
+let name = "divyanshu"
+let age = 18
+let isLOGGedIn = false
+let state; //this is the example of undefined
+let state= null//prestantation ofempty value 
+
+//some of data type present in javascript
+//number => 2 to power 53
+// bigint +> used for big numsber
+// string => ""
+//boolean => true/false
+//null => standaline
+// symbol => uniquness ke lye use hota hai
+
+
+// object
+
+console.log(typeof null)
