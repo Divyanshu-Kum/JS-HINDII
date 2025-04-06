bba03dd7f83cdf83b878e28d92341d6bba615c60
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-04-06 13:56:39 +0000

    conversionoperation

---
+++ .devcontainer/01-Basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = true
+
+//console.log(typeof (score));
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+
+
+//console.log(typeof(valueInNumber));
+
+//console.log(valueInNumber);
+
+// "33" == 33
+// "33adfsdf" == NaN
+// true = 1; false = 0
+
+let isLOGGedIn = "divyanshy"// convert 1 into boolean
+
+let booleanIsLoggedIn = Boolean(isLOGGedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true; 0= false
+// "" = false
+// "hitesh" = true
+
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
