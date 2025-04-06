e5e27f11df300bf629d427dd10401024caf94aec
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-04-06 14:52:30 +0000

    comparison

--- .devcontainer/01-Basics/03_conversionoperation.js
+++ .devcontainer/01-Basics/03_conversionoperation.js
@@ -17,15 +17,31 @@ let score = true
 let isLOGGedIn = "divyanshy"// convert 1 into boolean
 
 let booleanIsLoggedIn = Boolean(isLOGGedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 = true; 0= false
 // "" = false
 // "hitesh" = true
 
 
-let somenumber = 33
+//let somenumber = 33
 
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//let stringnumber = String(somenumber)
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+// ****************** Operations *****************************
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " divyanshu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
