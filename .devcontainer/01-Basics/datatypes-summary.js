806333df8b904fafa288ca029683dd7926ce3344
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-04-06 18:23:37 +0000

    datatype-summary

---
+++ .devcontainer/01-Basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLOGGedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bignumber = 987654321
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Akshay", "Amitab Bachan", "Karan"]
+
+let myObj = {
+    name: "divyanshu",
+Agg: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId)
