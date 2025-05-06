f0d2fe2f88a799d91c0f1b570e60f8dfb693cc27
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-05-06 17:18:52 +0530

    brother

--- 01-Basics/03-Basics/01-functions.js
+++ 01-Basics/03-Basics/01-functions.js
@@ -21,4 +21,49 @@ function addTwoNumbers(number1, number2){
 }
 
 const result = addTwoNumbers(3, 5)
-console.log("Result: ", result);
+//console.log("Result: ", result);
+
+function loginUserMessage(username){
+    if(username == undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+//console.log(loginUserMessage("divyanshu"))
+// console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is $
+        {anyobject.price}`);
+}
+
+//handleobject(user)
+handleobject({
+    username: "dev",
+    price: 378
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSeconfValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSeconfValue(myNewArray));
+console.log(returnSeconfValue(myNewArray));
+
+
+
