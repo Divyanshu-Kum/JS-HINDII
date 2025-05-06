fde6b3f5c9c757d1e831b355c08849fa00840637
Divyanshu-Kum <97398925+Divyanshu-Kum@users.noreply.github.com>
2025-05-06 11:50:50 +0530

    boy

---
+++ 01-Basics/03-Basics/01-functions.js
@@ -0,0 +1,24 @@
+
+function sayMyName(){
+    console.log("D");
+console.log("D");
+
+console.log("D");
+console.log("D");
+
+}
+// sayMyName()
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let return = number1 + number2
+    // return result 
+    return number1 + number2
+    
+}
+
+const result = addTwoNumbers(3, 5)
+console.log("Result: ", result);
